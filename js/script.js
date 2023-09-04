0265071093e9babf7524d313ae77ee21b795162f
hisham3amer <111225608+hisham3amer@users.noreply.github.com>
2023-09-05 02:54:18 +0300

    js update

--- js/script.js
+++ js/script.js
@@ -1,13 +1,17 @@
+let text = "25";
 $("i.fa-heart").click(changeClass);
 
 function changeClass() {
     if($(this).hasClass('fa-thin')){
         $(this).removeClass("fa-thin").addClass("fa-solid");
+        document.$("this+ span").innerText = 25;
     }
     else if($("i.fa-heart").hasClass('fa-solid')){
         $(this).removeClass("fa-solid").addClass("fa-thin");
+        document.getElementById("icon").innerText = 24;
     }
 
     
 }
+// document.getElementById("icon").innerText = text;
 
