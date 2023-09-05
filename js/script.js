33e095883530f6945990ae1dbef49753e07b46f1
hisham3amer <meshoamer94@gmail.com>
2023-09-05 12:09:12 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,14 +4,12 @@ $("i.fa-heart").click(changeClass);
 function changeClass() {
     if($(this).hasClass('fa-thin')){
         $(this).removeClass("fa-thin").addClass("fa-solid");
-        document.$("this+ span").innerText = 25;
+        document.getElementById("icon").innerText = 25;
     }
     else if($("i.fa-heart").hasClass('fa-solid')){
         $(this).removeClass("fa-solid").addClass("fa-thin");
         document.getElementById("icon").innerText = 24;
-    }
-
-    
+    } 
 }
-// document.getElementById("icon").innerText = text;
+
 
